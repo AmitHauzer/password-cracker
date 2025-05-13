a94099d298864c3cb0ecf53f192457b800f6e3a8
Amit Hauzer <amit.hauzer@gmail.com>
2025-05-13 19:04:02 +0300

    fix crack_range log

--- src/utils/minion_utils.py
+++ src/utils/minion_utils.py
@@ -8,7 +8,7 @@ from logging import getLogger
 import httpx
 from pydantic import ValidationError
 
-from config import FORMATTER_TASK_NAME, MASTER_SERVER_URL, MINION_SERVER_LOGGER
+from config import CANCEL_CHECK_INTERVAL, FORMATTER_TASK_NAME, LOG_PROGRESS_INTERVAL, MASTER_SERVER_URL, MINION_SERVER_LOGGER
 from formatters import FORMATTERS
 from models.schemas.request import SubmitResultRequest
 from models.schemas.response import GetTaskResponse
@@ -49,27 +49,25 @@ async def submit_result(minion_id: str, task_id: str, result: str) -> None:
 
 async def crack_range(minion_id: str, task_id: str, hash_value: str, start: int, end: int) -> None:
     """Crack a range of numbers."""
-    logger.info(
-        f"[{task_id}] Starting crack: hash={hash_value},range={start}-{end}")
 
     fmt = FORMATTERS[FORMATTER_TASK_NAME]
     total = end - start + 1
     tried = 0
-    percent_checkpoint = max(total // 10, 1)  # every 10%
+
+    logger.info(
+        f"[{task_id}] - Starting crack: hash={hash_value},range={fmt.number_to_string(start)}-{fmt.number_to_string(end)}")
 
     for candidate in range(start, end + 1):
         tried += 1
-
         phone_str = fmt.number_to_string(candidate)
 
-        # every 10% of the range, log progress
-        if tried % percent_checkpoint == 0:
+        if tried % LOG_PROGRESS_INTERVAL == 0:
             pct = (tried / total) * 100
             logger.info(
-                f"[{task_id}] Progress: {tried}/{total}, ({pct:.1f}%) — last={phone_str}")
+                f"[{task_id}] - Progress: ({pct:.1f}%) current={phone_str}")
 
         # every N attempts (or time), check if we should stop:
-        if candidate % 1000 == 0:
+        if candidate % CANCEL_CHECK_INTERVAL == 0:
             if not await should_continue(task_id):
                 logger.info(f"Task {task_id} cancelled—stopping early.")
                 return  # exit the loop
@@ -77,7 +75,7 @@ async def crack_range(minion_id: str, task_id: str, hash_value: str, start: int,
         if md5(phone_str.encode()).hexdigest() == hash_value:
             # found it—report and return
             logger.info(
-                f"[{task_id}] - FOUND Password: {phone_str}.")
+                f"[{task_id}] - FOUND Password!: password={phone_str}, hash={hash_value}")
             await submit_result(minion_id, task_id, phone_str)
             return
 
